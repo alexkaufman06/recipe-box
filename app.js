5742a9102cba1a29a0b71a667914296e49cd203c
Alex Kaufman <alexkaufman06@gmail.com>
2015-05-22 14:58:07 -0700

    Add panel functionality

--- app.js
+++ app.js
@@ -11,11 +11,24 @@
     this.recipes = recipes;
   });
 
+  app.controller('PanelController', function() {
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
   // Hard coded variable for seed data.  Eventually this should be moved to an
   // actual database.
   var recipes = [{
     name: 'Soup',
-    description: 'A light soup with lots of flavor',
+    price: 15,
+    description: 'A heafty soup packed with flavor.  Chicken is initially cooked in olive oil after being seasoned with salt, pepper, and fresh rosemary.  Chicken is then added to pot of potatoes and vegetables.',
     ingredients: [
       "Chicken Stock",
       "Rosemary",
@@ -28,5 +41,23 @@
       createdOn: 1432229390407
     }],
     image: "http://1.bp.blogspot.com/-fySnQSbbqM0/Tnp9cuNKlFI/AAAAAAAAA5M/BdA6jRsC8Go/s1600/DSC_2565.JPG"
+    },
+    {
+     name: 'Chili',
+     price: 16,
+     description: 'A hearty chili',
+     ingredients: [
+       "Beef",
+       "Beans",
+       "Garlic",
+       "Onion"
+     ],
+     reviews: [{
+       stars: 5,
+       body: "Delicious chili straight from Momma's recipe",
+       author: "alex@gmail.com",
+       createdOn: 1432320624109
+     }],
+     image: "http://www.seriouseats.com/images/20120126-vegan-chili-1.jpg"
   }];
 })();
